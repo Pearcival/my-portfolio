600c110bd5c3fac1a11b96ab4dd23c9c888b72f8
Joshua P <74361525+Pearcival@users.noreply.github.com>
2023-01-12 08:01:22 +0000

    Further grammar improvements

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -16,10 +16,10 @@ const Home = () => (
                     </div>
                     <h1>A Software Developer from the UK.</h1>
                     <h3>
-                        Originally self-taught, I'm a Web Developer with 10 years experience and in-depth knowledge of a number of technologies and languages.
+                        Initially self-taught, I'm a Web Developer with ten years of experience and in-depth knowledge of several technologies and languages.
                     </h3>
                     <h3>
-                        I create applications that help customers solve business challenges and bring their technical strategy to life.
+                        I create applications that help customers solve business challenges and bring their technical strategies to life.
                     </h3>
                     <InlineNavigationItem
                         title="Learn more >"
