31c86fa0beedf5812a6d598178f73f2469c4123e
Joshua P <74361525+Pearcival@users.noreply.github.com>
2023-11-20 19:34:49 +0000

    Bug Fix - Mobile Menu now closes when changing from mobile to desktop view

--- src/components/navigation/navigationBar/NavigationBar.tsx
+++ src/components/navigation/navigationBar/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AnimatePresence, motion, useCycle } from "framer-motion";
 
@@ -29,6 +29,21 @@ const NavigationBar: React.FC<INavigationBarProps> = ({ currentPage }) => {
 
     const navigate = useNavigate();
     const [navigationMenuOpen, cycleNavigationMenuOpen] = useCycle<boolean>(false, true);
+
+    useEffect(() => {
+
+        const handleWindowResize = () => {
+            if (window.innerWidth > 600 && navigationMenuOpen) {
+                cycleNavigationMenuOpen();
+            }
+        }
+
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, [navigationMenuOpen, cycleNavigationMenuOpen]);
 
     return (
         <>
@@ -77,71 +92,71 @@ const NavigationBar: React.FC<INavigationBarProps> = ({ currentPage }) => {
                         active={navigationMenuOpen}
                         handleClick={cycleNavigationMenuOpen}
                     />
+                    <AnimatePresence>
+                        {navigationMenuOpen &&
+                            <motion.div
+                                className="NavigationMenu"
+                                initial={{ height: 0 }}
+                                animate={{ height: "auto" }}
+                                transition={{ duration: 0.7 }}
+                                exit={{
+                                    height: 0,
+                                    transition: { delay: 0.2, duration: 0.7 }
+                                }}
+                            >
+                                <motion.div
+                                    className="NavigationMenuItems"
+                                    initial="closed"
+                                    animate="open"
+                                    exit="closed"
+                                    variants={menuVariants}
+                                >
+                                    <NavigationBarItem
+                                        title="Home"
+                                        navigate={() => {
+                                            navigate("/");
+                                            cycleNavigationMenuOpen();
+                                        }}
+                                        selected={currentPage === "/"}
+                                    />
+                                    <NavigationBarItem
+                                        title="About"
+                                        navigate={() => {
+                                            navigate("/about");
+                                            cycleNavigationMenuOpen();
+                                        }}
+                                        selected={currentPage === "/about"}
+                                    />
+                                    <NavigationBarItem
+                                        title="Skills"
+                                        navigate={() => {
+                                            navigate("/skills");
+                                            cycleNavigationMenuOpen();
+                                        }}
+                                        selected={currentPage === "/skills"}
+                                    />
+                                    <NavigationBarItem
+                                        title="Work"
+                                        navigate={() => {
+                                            navigate("/work");
+                                            cycleNavigationMenuOpen();
+                                        }}
+                                        selected={currentPage === "/work"}
+                                    />
+                                    <NavigationBarItem
+                                        title="Contact"
+                                        navigate={() => {
+                                            navigate("/contact");
+                                            cycleNavigationMenuOpen();
+                                        }}
+                                        selected={currentPage === "/contact"}
+                                    />
+                                </motion.div>
+                            </motion.div>
+                        }
+                    </AnimatePresence>
                 </div>
             </div>
-            <AnimatePresence>
-                {navigationMenuOpen &&
-                    <motion.div
-                        className="NavigationMenu"
-                        initial={{ height: 0 }}
-                        animate={{ height: "auto" }}
-                        transition={{ duration: 0.7 }}
-                        exit={{
-                            height: 0,
-                            transition: { delay: 0.2, duration: 0.7 }
-                        }}
-                    >
-                        <motion.div
-                            className="NavigationMenuItems"
-                            initial="closed"
-                            animate="open"
-                            exit="closed"
-                            variants={menuVariants}
-                        >
-                            <NavigationBarItem
-                                title="Home"
-                                navigate={() => {
-                                    navigate("/");
-                                    cycleNavigationMenuOpen();
-                                }}
-                                selected={currentPage === "/"}
-                            />
-                            <NavigationBarItem
-                                title="About"
-                                navigate={() => {
-                                    navigate("/about");
-                                    cycleNavigationMenuOpen();
-                                }}
-                                selected={currentPage === "/about"}
-                            />
-                            <NavigationBarItem
-                                title="Skills"
-                                navigate={() => {
-                                    navigate("/skills");
-                                    cycleNavigationMenuOpen();
-                                }}
-                                selected={currentPage === "/skills"}
-                            />
-                            <NavigationBarItem
-                                title="Work"
-                                navigate={() => {
-                                    navigate("/work");
-                                    cycleNavigationMenuOpen();
-                                }}
-                                selected={currentPage === "/work"}
-                            />
-                            <NavigationBarItem
-                                title="Contact"
-                                navigate={() => {
-                                    navigate("/contact");
-                                    cycleNavigationMenuOpen();
-                                }}
-                                selected={currentPage === "/contact"}
-                            />
-                        </motion.div>
-                    </motion.div>
-                }
-            </AnimatePresence>
         </>
     );
 };
