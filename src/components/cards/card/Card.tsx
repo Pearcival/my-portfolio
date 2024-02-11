7a223e045a2d7fb14288bc1145535187913a80f4
Joshua P <74361525+Pearcival@users.noreply.github.com>
2024-02-11 21:31:49 +0000

    Changes to card animation

--- src/components/cards/card/Card.tsx
+++ src/components/cards/card/Card.tsx
@@ -8,25 +8,18 @@ const Card: React.FC<ICard> = ({
     name,
     pathToLogo,
     invertedBackground,
-}) => {
-    const initialX = (Math.floor(Math.random() * (Math.floor(100) - Math.ceil(-100) + 1)) + Math.ceil(-100)) * 10;
-    const initialY = (Math.floor(Math.random() * (Math.floor(100) - Math.ceil(-100) + 1)) + Math.ceil(-100)) * 10;
-
-    return (
-        <motion.div
-            initial={{ x: initialX, y: initialY, opacity: 0 }}
-            transition={{ duration: 1, delay: Math.random() }}
-            animate={{
-                x: 0,
-                y: 0,
-                opacity: 1,
-            }}
-            className="Card"
-        >
-            <img className={`CardImage ${invertedBackground ? "White" : ""}`} src={pathToLogo} alt={name} />
-            <p style={{ margin: "0.5rem", textAlign: "center" }}>{name}</p>
-        </motion.div>
-    );
-};
+}) => (
+    <motion.div
+        initial={{ rotateX: 90, opacity: 0 }}
+        transition={{ duration: 1.5, delay: Math.random() }}
+        animate={{
+            rotateX: 0, opacity: 1,
+        }}
+        className="Card"
+    >
+        <img className={`CardImage ${invertedBackground ? "White" : ""}`} src={pathToLogo} alt={name} />
+        <p style={{ margin: "0.5rem", textAlign: "center" }}>{name}</p>
+    </motion.div>
+);
 
 export default Card;
